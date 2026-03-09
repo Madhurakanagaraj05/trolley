aeb822d0cf39b6cc39daa5be5177fd8f494c1fc7
MADHURA <madhurakanagaraj05@gmail.com>
2026-03-09 12:35:00 +0530

    added createOrder API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -14,12 +14,21 @@ export async function login(username, password) {
   const res = await fetch(`${API_BASE}/api/login`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      username: username,
-      password: password,
-    }),
+    body: JSON.stringify({ username, password })
+  });
+
+  return res.json();
+}
+
+export async function createOrder(orderData) {
+  const res = await fetch(`${API_BASE}/api/orders`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(orderData)
   });
 
   return res.json();
